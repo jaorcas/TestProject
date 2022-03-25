9084c356c2c937cb3a91aa501e530c1f0169eb8e
jaorc <jaorc@DESKTOP-P09U3FO>
2022-03-25 23:42:31 +0100

    Commit Print

--- GitHubTest/src/main.java
+++ GitHubTest/src/main.java
@@ -3,7 +3,7 @@ public class main {
 
 	public static void main (String args[]) {
 	
-		System.out.print("En este proyecto trabajamos con repositorio GITHUB");
+		System.out.print("En este proyecto trabajamos con el repositorio GITHUB");
 	}
 	
 }
