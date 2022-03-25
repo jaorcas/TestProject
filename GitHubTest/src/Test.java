9ebf4153ad39e3ba2d46dbbb570c47cce18030d2
jaorcas <jaorcas_10@hotmail.com>
2022-03-25 23:50:29 +0100

    Update Test.java

--- GitHubTest/src/Test.java
+++ GitHubTest/src/Test.java
@@ -4,6 +4,7 @@ public class Test {
 	public static void Test (String args[]){
 		
 		System.out.print("En este proyecto trabajamos con el repositorio GITHUB");
+		System.out.print("En este proyecto trabajamos con el repositorio GITHUB 2");
 		
 	}
 }
